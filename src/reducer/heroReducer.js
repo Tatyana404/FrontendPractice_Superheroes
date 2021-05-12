36286d9f95377cc0fed9dcd4a5e1bd67e8d321cf
Tatyana404 <kot.kate2020@gmail.com>
2021-05-12 15:51:05 +0300

    Add to hero reduser get heroes

--- src/reducer/heroReducer.js
+++ src/reducer/heroReducer.js
@@ -8,6 +8,34 @@ const initialState = {
 
 function heroReducer (state = initialState, action) {
   switch (action.type) {
+    case ACTION_TYPES.GET_HERO_REQUEST: {
+      return {
+        ...state,
+        isFetching: true,
+      };
+    }
+    case ACTION_TYPES.GET_HERO_SUCCESS: {
+      const {
+        payload: { heroes: newHeroes },
+      } = action;
+      const { heroes } = state;
+      return {
+        ...state,
+        isFetching: false,
+        error: null,
+        heroes: [...heroes, ...newHeroes],
+      };
+    }
+    case ACTION_TYPES.GET_HERO_ERROR: {
+      const {
+        payload: { error },
+      } = action;
+      return {
+        ...state,
+        isFetching: false,
+        error,
+      };
+    }
     case ACTION_TYPES.CREATE_HERO_REQUEST: {
       return {
         ...state,
@@ -16,10 +44,12 @@ function heroReducer (state = initialState, action) {
     }
     case ACTION_TYPES.CREATE_HERO_SUCCESS: {
       const { values: hero } = action;
+      const { heroes } = state;
       return {
         ...state,
         isFetching: false,
-        heroes: [...state.heroes, hero],
+        error: null,
+        heroes: [...heroes, hero],
       };
     }
     case ACTION_TYPES.CREATE_HERO_ERROR: {
